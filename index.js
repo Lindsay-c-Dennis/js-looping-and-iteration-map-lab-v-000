0b4efcdc56d40d7e6f2185cef51ecfc23813d5be
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-23 19:59:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ function nameToAttributes(drivers) {
 }
 
 function attributesToPhrase(drivers) {
-  
+  const reformattedArray = drivers.map(obj =>{ 
+   var rObj = {};
+   rObj[obj.key] = obj.value;
+   return rObj;
 }
